e733015a89bd1213d4ca635b7d01726caaeb1e48
Ali <ali.daoudi@hotmail.fr>
2018-10-06 19:23:48 -0600

    Added random room creation algorithm

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,6 +1,6 @@
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
-
+from random import randint
 class GameMap:
     def __init__(self, width, height):
         self.width = width
@@ -27,13 +27,37 @@ class GameMap:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
-    def make_map(self):
-        room1 = Rect(20, 15, 10, 15)
-        room2 = Rect(35, 15, 10, 15)
-
-        self.create_room(room1)
-        self.create_room(room2)                
-        self.create_h_tunnel(25,40,23)
+    def make_map(self,max_rooms,room_min_size,room_max_size,map_width,map_height,player):
+        rooms = []
+        num_rooms = 0 
+        for r in range(max_rooms):
+            w = randint(room_min_size,room_max_size)
+            h = randint(room_min_size,room_max_size)
+            x = randint(0,map_width-w-1)
+            y = randint(0,map_height-h-1)
+            new_room = Rect(x,y,w,h)
+            for other_room in rooms : 
+                print("lol")
+                if new_room.intersect(other_room):
+                    break
+                else :
+                    print("a")
+                    self.create_room(new_room)
+                    (new_x,new_y)=new_room.center()
+                    if num_rooms == 0 :
+                        player.x=new_x
+                        player.y=new_y
+                    else :
+                        (prev_x,prev_y) = rooms[num_rooms - 1].center()
+                        if randint(0,1)==1 : 
+                            self.create_h_tunnel(prev_x,new_x,prev_y)
+                            self.create_v_tunnel(prev_y,new_y,new_x)
+                        else :
+                            self.create_v_tunnel(prev_y,new_y,prev_x)
+                            self.create_h_tunnel(prev_x,new_x,new_y)
+            rooms.append(new_room)
+            num_rooms +=1
+        
     def is_blocked(self,x,y):
         if self.tiles[x][y].blocked == True :
             return True
